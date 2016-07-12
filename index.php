36a2fefd360ab47e0908fadd96b40d83f39e7920
Victoria Chacon <Vchaco06@gmail.com>
2016-07-12 14:56:51 -0600

    added tags and tables

--- index.php
+++ index.php
@@ -5,7 +5,48 @@
 		<title>My.Web.Page</title>
 	</head>
 	<body>
-		<h1>Hi World :D </h1>
+		<header>
+			<h1>Hi World :D </h1>
+		</header>
+		<main>
+			<p> id="first-paragraph">Galão qui <strong>strong</strong> foam black iced siphon <em>organic</em> caffeine plunger pot. Affogato instant kopi-luwak half and half sugar black single shot. Brewed aftertaste et, aftertaste sugar cinnamon, body sugar ristretto aroma iced. Ut, variety fair trade arabica lungo ut, cappuccino instant single shot and sugar.
+
+				And single shot, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic. Arabica blue mountain, robusta, mocha, aftertaste ut sweet fair trade white.</p>
+			<h2> class="pretty-title red-text">About Me</h2>
+			<p>, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic</p>
+			<ul>
+				<li>Starbucks</li>
+				<li>Home-made</li>
+				<li>instant</li>
+			</ul>
+			<ol>
+				<li>Starbucks</li>
+				<li>Home-made</li>
+				<li>instant</li>
+			</ol>
+			<div>
+				<a href="https://bootcamp-coders.cnm.edu/" target="_blank">Go to CNM Coding Site </a>
+			</div>
+			<img src="images/Westie.jpg" alt="Westie" />
+			<table>
+				<tr>
+					<th>
+						table cell 1
+					</th>
+					<th>
+						table cell 2
+					</th>
+				</tr>
+				<tr>
+					<td>
+						table cell 3
+					</td>
+					<td>
+						table cell 4
+					</td>
+				</tr>
+			</table>
+		</main>
 		<p>Galão qui strong foam black iced siphon organic caffeine plunger pot. Affogato instant kopi-luwak half and half sugar black single shot. Brewed aftertaste et, aftertaste sugar cinnamon, body sugar ristretto aroma iced. Ut, variety fair trade arabica lungo ut, cappuccino instant single shot and sugar.
 
 			And single shot, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic. Arabica blue mountain, robusta, mocha, aftertaste ut sweet fair trade white.</p>
@@ -25,6 +66,28 @@
 		<a href="https://bootcamp-coders.cnm.edu/" target="_blank">Go to CNM Coding Site </a>
 		</div>
 		<img src="images/Westie.jpg" alt="Westie" />
+		<table>
+			<tr>
+				<th>
+					table cell 1
+				</th>
+				<th>
+					table cell 2
+				</th>
+			</tr>
+			<tr>
+				<td>
+					table cell 3
+				</td>
+				<td>
+					table cell 4
+				</td>
+			</tr>
+		</table>
+
+		<footer>
+			&copy; 2016
+		</footer>
 	</body>
 	
 </html>
