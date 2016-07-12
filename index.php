da064d91210c429f870fc2681fb916d9b50b32b4
Victoria Chacon <Vchaco06@gmail.com>
2016-07-12 12:51:47 -0600

    added paragraphs, lists, and divs.

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
 	</head>
 	<body>
 		<h1>Hi World :D </h1>
+		<p>Galão qui strong foam black iced siphon organic caffeine plunger pot. Affogato instant kopi-luwak half and half sugar black single shot. Brewed aftertaste et, aftertaste sugar cinnamon, body sugar ristretto aroma iced. Ut, variety fair trade arabica lungo ut, cappuccino instant single shot and sugar.
 
+			And single shot, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic. Arabica blue mountain, robusta, mocha, aftertaste ut sweet fair trade white.</p>
+		<h2>About Me</h2>
+		<p>, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic</p>
+		<ul>
+			<li>Starbucks</li>
+			<li>Home-made</li>
+			<li>instant</li>
+		</ul>
+		<ol>
+			<li>Starbucks</li>
+			<li>Home-made</li>
+			<li>instant</li>
+		</ol>
+		<div>
+		<a href="https://bootcamp-coders.cnm.edu/" target="_blank">Go to CNM Coding Site </a>
+			</div>
 	</body>
 </html>
