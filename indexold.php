92ab45e7ddcecad21b3ac9d5a0f7193ecc934ead
Victoria Chacon <Vchaco06@gmail.com>
2016-08-04 08:54:15 -0600

    added a new index.php project. Your other html css is in indexold.php

---
+++ indexold.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="'en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<!--adding custom css-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<!--adding custom javascript-->
+		<script src="js/custom.js" type="text/javascript"></script>
+		<title>My.Web.Page</title>
+	</head>
+	<body>
+		<!--Begin Header-->
+		<header>
+			<h1>Hi World :D </h1>
+		</header>
+		<!--Start Main Content Here-->
+		<main>
+			<!--Comment Text Goes Here-->
+			<p id="first-paragraph">Galão qui <strong>strong</strong> foam black iced siphon <em>organic</em> caffeine plunger pot. Affogato instant kopi-luwak half and half sugar black single shot. Brewed aftertaste et, aftertaste sugar cinnamon, body sugar ristretto aroma iced. Ut, variety fair trade arabica lungo ut, cappuccino instant single shot and sugar.
+
+				And single shot, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic. Arabica blue mountain, robusta, mocha, aftertaste ut sweet fair trade white.</p>
+			<h1 class="pretty-title red-text">About Me</h1>
+			<p>, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic</p>
+			<ul>
+				<li>Starbucks</li>
+				<li>Home-made</li>
+				<li>instant</li>
+			</ul>
+			<ol>
+				<li>Starbucks</li>
+				<li>Home-made</li>
+				<li>instant</li>
+			</ol>
+			<div>
+				<a href="https://bootcamp-coders.cnm.edu/" target="_blank">Go to CNM Coding Site </a>
+			</div>
+			<img src="images/Westie.jpg" alt="Westie" />
+			<table>
+				<tr>
+					<th>
+						table cell 1
+					</th>
+					<th>
+						table cell 2
+					</th>
+				</tr>
+				<tr>
+					<td>
+						table cell 3
+					</td>
+					<td>
+						table cell 4
+					</td>
+				</tr>
+			</table>
+		</main>
+		<p>Galão qui strong foam black iced siphon organic caffeine plunger pot. Affogato instant kopi-luwak half and half sugar black single shot. Brewed aftertaste et, aftertaste sugar cinnamon, body sugar ristretto aroma iced. Ut, variety fair trade arabica lungo ut, cappuccino instant single shot and sugar.
+
+			And single shot, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic. Arabica blue mountain, robusta, mocha, aftertaste ut sweet fair trade white.</p>
+		<h2>About Me</h2>
+		<p>, blue mountain french press, caramelization cultivar mug filter grinder. Cappuccino spoon, at flavour and organic to go mug whipped at aged organic</p>
+		<ul>
+			<li>Starbucks</li>
+			<li>Home-made</li>
+			<li>instant</li>
+		</ul>
+		<ol>
+			<li>Starbucks</li>
+			<li>Home-made</li>
+			<li>instant</li>
+		</ol>
+		<div>
+		<a href="https://bootcamp-coders.cnm.edu/" target="_blank">Go to CNM Coding Site </a>
+		</div>
+		<img src="images/Westie.jpg" alt="Westie" />
+		<table>
+			<tr>
+				<th>
+					table cell 1
+				</th>
+				<th>
+					table cell 2
+				</th>
+			</tr>
+			<tr>
+				<td>
+					table cell 3
+				</td>
+				<td>
+					table cell 4
+				</td>
+			</tr>
+		</table>
+			<!--Begin footer here-->
+		<footer>
+			&copy; 2016
+		</footer>
+	</body>
+	
+</html>
